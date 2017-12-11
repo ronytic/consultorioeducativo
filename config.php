e8c8fb47a72f9c0e02a5c5b20020c3f9f2c1c713
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2017-12-11 12:31:43 -0400

    Config

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 $title="Sistema Consultorio Educativo Tecnologico Y Psicopedagogico de Apoyo Escolar";
 $siglas="";
 $directory="consultorioeducativo";//ej:carp/ sin el "/"
-$url="http://localhost/";//ej:http:"localhost/"
+$url="http://11.1.2.31/";//ej:http:"localhost/"
 $lema2="";
 $lema='Sistema de Educación de Calidad';
-$direccion="EDIFICIO CHUQUIAGO CALLLE MEXICO Nro 1551 P.B. OF 1 TEL: 2114043 - 70642896";
+$direccion="El Alto Zona Villa Bolivar";
 $telefono="2482516";
 
 $ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
